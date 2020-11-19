9678f00567b5a1b4f076744563cffc07a673385b
Ben Moran <benmoran@stanford.edu>
2020-11-18 17:56:16 -0800

    Update 1.10.1_loop_bootstrap_psmc.sh

--- psmc_documentation/1.10.1_loop_bootstrap_psmc.sh
+++ psmc_documentation/1.10.1_loop_bootstrap_psmc.sh
@@ -5,5 +5,5 @@ splitfa cor_5587-MS-0004_mapped_bir.psmcfa 5000 > cor_5587-MS-0004_mapped_bir_sp
 
 for i in {1..100}; do
     echo $i
-    sed "s/XXrepXX/$i/g" bootstrap_psmc_cor_mapped_bir.sh | sbatch
+    sed "s/XXrepXX/$i/g" 1.10.2_bootstrap_psmc_cor_mapped_bir.sh | sbatch
 done
