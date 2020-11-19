13414ed88d6b66c312b0b753522a616b5a0ac938
Ben Moran <benmoran@stanford.edu>
2020-11-18 16:13:20 -0800

    Update 1.3_realign_cor_mapped_bir.sh

--- psmc_documentation/1.3_realign_cor_mapped_bir.sh
+++ psmc_documentation/1.3_realign_cor_mapped_bir.sh
@@ -7,13 +7,13 @@
 #SBATCH --mem=64000
 #SBATCH -o slurm-%x.out
 
-java -jar /home/groups/schumer/shared_bin/GenomeAnalysisTK.jar \
+java -jar GenomeAnalysisTK.jar \
 -T RealignerTargetCreator \
 -R xiphophorus_birchmanni_10x_12Sep2018_yDAA6.fasta \
 -I cor_5587-MS-0004_mapped_bir-dedup.sorted.bam \
 -o target_intervals_cor_5587-MS-0004.list
 
-java -jar /home/groups/schumer/shared_bin/GenomeAnalysisTK.jar \
+java -jar GenomeAnalysisTK.jar \
 -T IndelRealigner \
 -R xiphophorus_birchmanni_10x_12Sep2018_yDAA6.fasta \
 -I cor_5587-MS-0004_mapped_bir-dedup.sorted.bam \
