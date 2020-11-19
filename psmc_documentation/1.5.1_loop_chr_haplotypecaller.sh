92382ef6acee5615bb84fb30c5d4ef3c6934a642
Ben Moran <benmoran@stanford.edu>
2020-11-18 17:46:51 -0800

    Update 1.5.1_loop_chr_haplotypecaller.sh

--- psmc_documentation/1.5.1_loop_chr_haplotypecaller.sh
+++ psmc_documentation/1.5.1_loop_chr_haplotypecaller.sh
@@ -2,5 +2,5 @@
 
 for line in $(cat xbir-10x_chromlist.list); do
     echo $line
-    sed "s/XXchrXX/$line/g" haplotypecaller_ref_bir.sh | sbatch
+    sed "s/XXchrXX/$line/g" 1.5.2_haplotypecaller_ref_bir.sh | sbatch
 done
